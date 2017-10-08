4ef59aa4d9e7e09b6b50f69deb444a0299e84061
yan <yanxianwei1114@gmail.com>
2017-10-08 13:48:47 +0800

    didi 模式 适配recyclerView

--- app/src/main/java/com/yan/refreshloadlayouttest/widget/DiDiHeader.java
+++ app/src/main/java/com/yan/refreshloadlayouttest/widget/DiDiHeader.java
@@ -120,11 +120,7 @@ public class DiDiHeader extends FrameLayout implements PullRefreshLayout.OnPullL
         return new RecyclerView.OnScrollListener() {
             @Override
             public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-                View offsetView = getChildAt(0);
-                offsetView.setTranslationY(offsetView.getTranslationY() - dy > 0
-                        ? 0 :
-                        offsetView.getTranslationY() - dy
-                );
+                getChildAt(0).setTranslationY(Math.max(Math.min(0, getChildAt(0).getTranslationY() - dy), -fixedHeader.getHeight()));
             }
         };
     }
